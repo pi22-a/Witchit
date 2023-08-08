9daaea0c9409341350de36f4a42b116b037322aa
alta521 <alta521521@gmail.com>
2023-08-08 19:18:42 +0900

    08081918

--- Assets/02_Environment/01_Scripts/PotalIN.cs
+++ Assets/02_Environment/01_Scripts/PotalIN.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class PotalIN : MonoBehaviour
 {
     public Transform PotalPosition;
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider _col)
     {
-        Transform ParentTransform = other.transform;
-        while (true)
+        Debug.Log("Ãæµ¹");
+        if (_col.gameObject.tag == "Hunter")
         {
+            Transform ParentTransform = _col.transform;
+            while (true)
+            {
+                if (ParentTransform.parent == null) break;
+                else ParentTransform = ParentTransform.parent;
+            }
+            ParentTransform.position = PotalPosition.position;
+            ParentTransform.rotation = PotalPosition.rotation;
 
         }
+
     }
 }
