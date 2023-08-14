1ce6474445dd1b64052eef092b24abb967fc2071
alta521 <alta521521@gmail.com>
2023-08-14 13:50:07 +0900

    lll

--- Assets/02_Environment/01_Scripts/Lantern.cs
+++ Assets/02_Environment/01_Scripts/Lantern.cs
@@ -5,33 +5,34 @@ using UnityEngine;
 public class Lantern : MonoBehaviour
 {
     public GameObject player1;
-    public GameObject player2;
+    //public GameObject player2;
     public GameObject lightObject;
-    bool lightONOFF = false;
+    bool lightONOFF;
     public float maxRaycastDistance = 3f;
 
     void Update()
     {
         Vector3 directionToPlayer = player1.transform.position - transform.position;
-        Vector3 directionToPlayer2 = player2.transform.position - transform.position;
+        //Vector3 directionToPlayer2 = player2.transform.position - transform.position;
         float distanceToPlayer = directionToPlayer.magnitude;
-        float distanceToPlayer2 = directionToPlayer2.magnitude;
+        //float distanceToPlayer2 = directionToPlayer2.magnitude;
 
         RaycastHit hit;
         if (Physics.Raycast(transform.position, directionToPlayer, out hit, maxRaycastDistance))
         {
             if (hit.collider.gameObject == player1 && Input.GetKeyDown(KeyCode.E))
             {
+                Debug.Log("dd");
                 ToggleLantern();
             }
         }
-        if (Physics.Raycast(transform.position, directionToPlayer2, out hit, maxRaycastDistance))
-        {
-            if (hit.collider.gameObject == player2 && Input.GetKeyDown(KeyCode.E))
-            {
-                ToggleLantern();
-            }
-        }
+        //if (Physics.Raycast(transform.position, directionToPlayer2, out hit, maxRaycastDistance))
+        //{
+        //    if (hit.collider.gameObject == player2 && Input.GetKeyDown(KeyCode.E))
+        //    {
+        //        ToggleLantern();
+        //    }
+        //}
     }
 
     void ToggleLantern()
