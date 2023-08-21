b57e73f6d6c3a4339b8416e5e2494d0a79e6eb27
alta521 <alta521521@gmail.com>
2023-08-21 10:14:29 +0900

    821
    .

--- Assets/02_Environment/01_Scripts/PotalIN.cs
+++ Assets/02_Environment/01_Scripts/PotalIN.cs
@@ -4,69 +4,58 @@ using UnityEngine;
 
 public class PotalIN : MonoBehaviour
 {
-    public Transform player;
-    //public Transform witch;
     public Transform PotalPosition;
+    public Transform player;
 
-    private bool IsOverlapping = false;
+    private List<Collider> overlappingColliders = new List<Collider>();
 
     private void Update()
     {
-        if (IsOverlapping)
+        foreach (Collider collider in overlappingColliders)
         {
             Debug.Log("D");
-            Vector3 portalToPlayer = player.position - transform.position;
-            //float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
-            //if (dotProduct < 0f)
-            //{
-            //Debug.Log("E");
+            Vector3 portalToPlayer = collider.transform.position - transform.position;
             float rotationdiff = -Quaternion.Angle(transform.rotation, PotalPosition.rotation);
-            rotationdiff += 180;
-            player.Rotate(Vector3.up, rotationdiff);
+            //rotationdiff += 180;
+            collider.transform.Rotate(Vector3.up, rotationdiff);
             Vector3 positionOffset = Quaternion.Euler(0f, rotationdiff, 0f) * portalToPlayer;
-            if (player.TryGetComponent<CharacterController>(out CharacterController CC))
+
+            CharacterController CC = collider.GetComponent<CharacterController>();
+            if (CC != null)
             {
                 Debug.Log("S");
                 CC.enabled = false;
-                Debug.Log(transform.position);
             }
-            player.position = PotalPosition.position + positionOffset;
+
+            collider.transform.position = PotalPosition.position + positionOffset;
+            //collider.transform. = PotalPosition.forward;
+
             if (CC != null)
             {
                 Debug.Log("s");
-                Debug.Log(transform.position);
-
                 CC.enabled = true;
             }
 
-            IsOverlapping = false;
             Debug.Log("dd");
-            //}
         }
+
+        overlappingColliders.Clear();
     }
+
     private void OnTriggerEnter(Collider other)
     {
-
-        IsOverlapping = true;
-        //if (other.tag == "Hunter")
-        //{
-        //    Debug.Log("Ãæµ¹");
-        //    Transform ParentTransform = other.transform;
-        //    while (true)
-        //    {
-        //        if (ParentTransform.parent == null) break;
-        //        else ParentTransform = ParentTransform.parent;
-        //    }
-        //    ParentTransform.position = PotalPosition.position;
-        //    ParentTransform.rotation = PotalPosition.rotation;
-
-        //}
-
+        if (!overlappingColliders.Contains(other))
+        {
+            overlappingColliders.Add(other);
+        }
     }
+
     private void OnTriggerExit(Collider other)
     {
-
-        IsOverlapping = false;
-
+        if (overlappingColliders.Contains(other))
+        {
+            overlappingColliders.Remove(other);
+        }
     }
 }
+
